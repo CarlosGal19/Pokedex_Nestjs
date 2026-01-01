1bb874a5a11205e16f363e6adc52296664ebd435
Carlos Galindo <utm22030587@utma.edu.mx>
2026-01-01 15:19:29 -0600

    feat: connect to mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+    PokemonModule,
+    MongooseModule.forRoot(
+      'mongodb://root:example@localhost:27017/pokedex-api?authSource=admin',
+    ), // CONNECT TO MONGODB WITH MONGOOSE
   ],
   controllers: [],
   providers: [],
