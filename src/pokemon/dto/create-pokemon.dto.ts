72bfddef0db544294a768b8ee665a31f35d66c21
Carlos Galindo <utm22030587@utma.edu.mx>
2026-01-02 13:24:56 -0600

    feat: create pokemon dtos

---
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -0,0 +1,12 @@
+import { IsInt, IsPositive, IsString, Min, MinLength } from 'class-validator';
+
+export class CreatePokemonDto {
+  @IsInt()
+  @Min(1)
+  @IsPositive()
+  readonly no: number;
+
+  @IsString()
+  @MinLength(3)
+  readonly name: string;
+}
