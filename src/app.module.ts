f7d3e6199246b6b0c0be0347eee8d445e6ed6eeb
Carlos Galindo <utm22030587@utma.edu.mx>
2026-01-04 14:56:51 -0600

    feat: validate environment variables with zod schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,11 +7,16 @@ import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { ConfigModule } from '@nestjs/config';
 import { envConfiguration } from './common/config/env.config';
+import { ZodEnvSchema } from './common/config/env.validation';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [envConfiguration],
+      // validationSchema: ZodEnvSchema, // This does not work with zod because zod does not have the method validate()
+      validate: (config) => {
+        return ZodEnvSchema.parse(config);
+      },
     }), // READ ENVIRONMENT VARIABLES
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
